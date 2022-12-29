c0b416b372cb083bf5e28f47064f23479a731d9f
cuneytkaan <cuneytyorta@gmail.com>
2022-12-29 11:53:00 +0300

    Odev10.sql

---
+++ Odev10.sql
@@ -0,0 +1,15 @@
+--1.Soru
+SELECT city.city, country.country FROM city
+LEFT JOIN country
+ON city.country_id=country.country_id;
+
+--2.Soru
+SELECT payment.payment_id, customer.first_name, customer.last_name
+FROM payment
+RIGHT JOIN customer
+ON payment.customer_id=customer.customer_id;
+
+--3.Soru
+SELECT rental_id, first_name, last_name FROM rental
+FULL JOIN customer
+ON rental.customer_id = customer.customer_id;
